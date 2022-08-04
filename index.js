d8b21cb0c8bad094a71148279c6e80f1c9726fbe
Superiorist <89704757+Superiorist@users.noreply.github.com>
2022-08-04 12:35:34 +0400

    Update "start func"
    Patched

--- index.js
+++ index.js
@@ -13,29 +13,36 @@ bot.setMyCommands ([
     {command: '/contact', description: 'Написать нам'},
 ]) 
 
-bot.on ("message",async msg =>{
-    const text = msg.text
-    const chatId = msg.chat.id;
-    const name = msg.chat.first_name;
-     console.log (msg)
-     
-     if (text==='/start'){
-       return bot.sendMessage(chatId, `Привет, ${name}! Хотите оформить заказ? 🧐`,{reply_markup: {inline_keyboard:keyboard}
-    }); 
 
-    }
-     if (text === '/info') {
-        return bot.sendMessage(chatId, `Вот наш сайт https://supitweb.com/ !`)
-     }
+const start = () => {
+    bot.on ("message",async msg =>{
+        const text = msg.text
+        const chatId = msg.chat.id;
+        const name = msg.chat.first_name;
+         console.log (msg)
+         
+         if (text==='/start'){
+           return bot.sendMessage(chatId, `Привет, ${name}! Хотите оформить заказ? 🧐`,{reply_markup: {inline_keyboard:keyboard}
+        }); 
+    
+        }
+         if (text === '/info') {
+            return bot.sendMessage(chatId, `Вот наш сайт https://supitweb.com/ !`)
+         }
+    
+         if (text === '/contact') {
+            return bot.sendMessage(chatId, `Email: sup.it@mail.ru, Telegram: @dozer_stoun, Inst:  `)
+         }
+        return bot.sendMessage(chatId,'Прости, но я тебя не понял! ☹️ Лучше этот парень ответит на твои вопросы - @dozer_stoun')
+    })
+    bot.on ('callback_query', msg =>{
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        bot.sendMessage(chatId, 'Мое дело предложить!😉 Можете нажать на кнопку меню,там много полезного ↙️↙️↙️ ')
+        console.log (msg)
+    })
+}
 
-     if (text === '/contact') {
-        return bot.sendMessage(chatId, `Email: sup.it@mail.ru, Telegram: @dozer_stoun, Inst:  `)
-     }
-    return bot.sendMessage(chatId,'Прости, но я тебя не понял! ☹️ Лучше этот парень ответит на твои вопросы - @dozer_stoun')
-})
-bot.on ('callback_query', msg =>{
-    const data = msg.data;
-    const chatId = msg.message.chat.id;
-    bot.sendMessage(chatId, 'Мое дело предложить!😉 Можете нажать на кнопку меню,там много полезного ↙️↙️↙️ ')
-    console.log (msg)
-})
+start ()
+
+
